e7627a0ea9606d89a4200dcdff62d9e9e2792438
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-06-27 02:11:37 -0700

    Remove unnecessary init and add template for tests

---
+++ genbankfilter/test/test_mash.py
@@ -0,0 +1,19 @@
+import tempfile
+import unittest
+
+class TestCurate(unittest.TestCase):
+
+    def test_sketch(args):
+        None
+
+
+    def setUp(self):
+        None
+
+
+    def tearDown(self):
+        None
+
+
+if __name__ == '__main__':
+    unittest.main()
