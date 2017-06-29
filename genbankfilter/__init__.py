7f9c40ac65f0df211e8fc22cbe658c5655d0d529
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-06-29 04:10:11 -0700

    Move filtering modules into main module
    Unneccesary nesting of files for such a small number of modules

--- genbankfilter/__init__.py
+++ genbankfilter/__init__.py
@@ -1,4 +1,4 @@
-import genbankfilter.filtering.mash as mash
+import genbankfilter.mash as mash
 import genbankfilter.get_resources as get_resources
 import genbankfilter.config as config
 import genbankfilter.curate as curate
