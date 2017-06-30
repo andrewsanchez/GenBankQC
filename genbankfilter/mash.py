07e6edf7190b464d4e0b23c55974052f775a7bc4
Andrew A Sanchez <andrew@Andrews-MacBook-Pro-2.local>
2017-06-30 16:56:43 +0200

    Create archive.py and move write_sketch_commands to it

--- genbankfilter/mash.py
+++ genbankfilter/mash.py
@@ -6,30 +6,6 @@ import argparse
 import glob
 import subprocess
 
-def write_sketch_commands(genbank_mirror, assembly_summary, new_genomes):
-
-    sketch_commands = os.path.join(genbank_mirror, ".info", "sketch_commands.txt")
-    if os.path.isfile(sketch_commands):
-        os.remove(sketch_commands)
-
-    with open(sketch_commands, 'a') as cmds:
-        for genome in new_genomes:
-            species_dir = assembly_summary.scientific_name.loc[genome]
-            # TODO: This is no good.  Make it more efficient.
-            fasta = os.path.join(genbank_mirror, species_dir, "{}*fasta".format(genome))
-            fasta = glob.glob(fasta)
-
-            try:
-                fasta = fasta[0]
-            except IndexError:
-                continue
-
-            sketch_dst = os.path.join(genbank_mirror, species_dir, "{}.msh".format(genome))
-            cmd = "/common/contrib/bin/mash-Linux64-v1.1.1/mash sketch {} -o {}\n".format(fasta, sketch_dst)
-            cmds.write(cmd)
-
-    return sketch_commands # get the line count of this file
-
 def sketch(genbank_mirror, assembly_summary, genome):
 
     # TODO: Using os.walk will avoid having to look up species_dir using assembly_summary
