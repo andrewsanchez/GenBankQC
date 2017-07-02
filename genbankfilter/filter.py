cc22ccf983e21f80e107f4422b30c1af7e71b2ff
Andrew A Sanchez <andrew@Andrews-MacBook-Pro-2.local>
2017-07-02 17:46:40 +0200

    Do away with run.py and refactor filter.py
    Move into filter.py and refact

--- genbankfilter/filter.py
+++ genbankfilter/filter.py
@@ -6,7 +6,6 @@ from shutil import rmtree, move
 from Bio import SeqIO
 from re import findall
 from pandas.io.parsers import EmptyDataError
-# from link_passed_genomes import make_passed_dir, link_passed_genomes
 
 # Move to config
 def clean_up(fasta_dir):
@@ -62,6 +61,7 @@ def pre_process_all(genbank_mirror):
         else:
             continue
 
+# Convenience
 def generate_stats_genbank(genbank_mirror):
 
     x = 1
@@ -77,27 +77,9 @@ def generate_stats_genbank(genbank_mirror):
             print("Generated stats for {} out of {}".format(x, len(all_genbank_species)))
             x += 1
 
-def clean_up_matrix(info, distance_matrix):
+def generate_stats(species_dir, dst_mx):
 
-    """
-    Set indices and headers to the accession ID's
-    """
-
-    new_index = []
-    new_columns = []
-    for i in distance_matrix.index:
-        name = i.split("/")[-1].strip(".fasta")
-        new_index.append(name)
-        new_columns.append(name)
-
-    distance_matrix.index = new_index
-    distance_matrix.columns = new_columns
-    distance_matrix.to_csv(os.path.join(info, "distance_matrix_all.csv"), sep="\t")
-
-def generate_fasta_stats(fasta_dir, distance_matrix):
-
-    info = os.path.join(fasta_dir, "info")
-    for root, dirs, files, in os.walk(fasta_dir):
+    for root, dirs, files, in os.walk(species_dir):
         file_names = []
         contig_totals = []
         assembly_sizes = []
@@ -131,27 +113,17 @@ def generate_fasta_stats(fasta_dir, distance_matrix):
 
         SeqDataSet = list(zip(n_counts, contig_totals, assembly_sizes))
         stats = pd.DataFrame(data=SeqDataSet, index=file_names, columns=["N_Count", "Contigs", "Assembly_Size"], dtype="float64")
-        mean_distances = distance_matrix.mean()
+        mean_distances = dst_mx.mean()
         stats["MASH"] = mean_distances
-        stats.to_csv(os.path.join(info, "stats.csv"), index_label="Accession")
 
         return stats
 
-def filter_med_ad(fasta_dir, stats, max_n_count, c_range, s_range, m_range):
+def filter_med_ad(species_dir, stats, max_n_count=100,
+                  c_range=3.5, s_range=3.5, m_range=3.5):
 
-    info = os.path.join(fasta_dir, "info")
     filter_ranges = "{}_{}_{}".format(c_range, s_range, m_range)
 
-    # Create new log files and remove old ones
-    passed_log = os.path.join(info, "passed_{}.csv".format(filter_ranges))
-    failed_log = os.path.join(info, "failed_{}.csv".format(filter_ranges))
-    summary = os.path.join(info, "summary_{}.csv".format(filter_ranges))
-    summary_df = pd.DataFrame()
-
-    files = [passed_log, failed_log, summary]
-    for f in files:
-        if os.path.isfile(f):
-            os.remove(f)
+    filter_df = pd.DataFrame()
 
     # Filter based on N's first
     passed_I = stats[stats["N_Count"] <= max_n_count ]
@@ -163,11 +135,11 @@ def filter_med_ad(fasta_dir, stats, max_n_count, c_range, s_range, m_range):
             failed["N_Count"][i] = stats["N_Count"][i]
         else:
             failed["N_Count"][i] = "passed"
-    summary_df.set_value(filter_ranges, "N's", len(failed_N))
+    filter_df.set_value(filter_ranges, "N's", len(failed_N))
 
     # Filter using special function for contigs
     if len(passed_I) > 5:
-        filter_contigs_results = filter_contigs(stats, passed_I, filter_ranges, c_range, failed, summary_df)
+        filter_contigs_results = filter_contigs(stats, passed_I, filter_ranges, c_range, failed, filter_df)
         passed_II = filter_contigs_results.passed
         failed_contigs = filter_contigs_results.failed
 
@@ -185,8 +157,8 @@ def filter_med_ad(fasta_dir, stats, max_n_count, c_range, s_range, m_range):
 
             assembly_lower = passed_II["Assembly_Size"].median() - assembly_dev_ref
             assembly_upper = passed_II["Assembly_Size"].median() + assembly_dev_ref
-            summary_df.set_value(filter_ranges, "Assembly_Size", len(failed_assembly_size))
-            summary_df.set_value(filter_ranges, "Assembly_Range", "{:.0f}-{:.0f}".format(assembly_lower, assembly_upper))
+            filter_df.set_value(filter_ranges, "Assembly_Size", len(failed_assembly_size))
+            filter_df.set_value(filter_ranges, "Assembly_Range", "{:.0f}-{:.0f}".format(assembly_lower, assembly_upper))
 
             if len(passed_III) > 5:
                 mash_med_ad = abs(passed_III["MASH"] - passed_III["MASH"].median()).mean()# Median absolute deviation
@@ -202,8 +174,8 @@ def filter_med_ad(fasta_dir, stats, max_n_count, c_range, s_range, m_range):
 
                 mash_lower = passed_II["MASH"].median() - mash_dev_ref
                 mash_upper = passed_II["MASH"].median() + mash_dev_ref
-                summary_df.set_value(filter_ranges, "MASH", len(failed_mash))
-                summary_df.set_value(filter_ranges, "MASH_Range", "{:04.3f}-{:04.3f}".format(mash_lower, mash_upper))
+                filter_df.set_value(filter_ranges, "MASH", len(failed_mash))
+                filter_df.set_value(filter_ranges, "MASH_Range", "{:04.3f}-{:04.3f}".format(mash_lower, mash_upper))
 
             else:
                 passed_final = passed_III
@@ -219,16 +191,77 @@ def filter_med_ad(fasta_dir, stats, max_n_count, c_range, s_range, m_range):
                 Filtering will not commence past this stage.".format(max_n_count))
 
     failed.drop(list(passed_final.index), inplace=True)
-    summary_df.set_value(filter_ranges, "Filtered", "{}/{}".format(len(failed), len(stats)))
-    summary_df.to_csv(summary, sep="\t", mode="a", header=True)
-    failed.to_csv(failed_log, na_rep="NA", header=True)
-    passed_final.to_csv(passed_log, header=True)
+    filter_df.set_value(filter_ranges, "Filtered", "{}/{}".format(len(failed), len(stats)))
+    return filter_df, failed, passed_final
 
-    filter_level = passed_log.split("/")[-1].strip(".csv")
-    passed_dir = os.path.join(fasta_dir, filter_level)
-    make_passed_dir(passed_dir, passed_log)
-    link_passed_genomes(fasta_dir, passed_dir, passed_log)
+def main():
+    parser = argparse.ArgumentParser(description = "Assess the integrity of your FASTA collection")
+    parser.add_argument("fasta_dir", help = "directory containing your FASTA files")
+    parser.add_argument("-d", "--directories", help = "The complete path to one or more directories containing\
+            FASTA's you want to run the filters on.", action="store_true")
+    parser.add_argument("-x", "--mash_exe", help = "Path to MASH")
+    parser.add_argument("-p", "--parent_dir", help = "The parent directory containing subdirectories with FASTA's for\
+            each of the collections you want to run the filters on.")
+    parser.add_argument("--from_list", help = "Specify a list of one more more directories to fun filters on.", nargs="+")
+    parser.add_argument("-n", "--max_n_count", help = "Maximum number of N's acceptable", type=int, default=100)
+    parser.add_argument("-c", "--c_range", help = "", type=float, default=3.5)
+    parser.add_argument("-s", "--s_range", help = "", type=float, default=3.5)
+    parser.add_argument("-m", "--m_range", help = "", type=float, default=3.5)
+    parser.add_argument("-l", "--filter_level", help = "Value to be used for all filters", type=float)
+    args = parser.parse_args()
 
+    def mash_stats_and_filter():
+        clean_up(fasta_dir)
+        assess_fastas(fasta_dir)
+        distance_matrix = mash(fasta_dir, mash_exe)
+        stats = generate_fasta_stats(fasta_dir, distance_matrix)
+        filter_med_ad(fasta_dir, stats, max_ns, c_range, s_range, m_range)
+
+    def just_filter():
+        stats = os.path.join(os.path.join(fasta_dir, "info"), "stats.csv")
+        stats = pd.read_csv(stats, index_col=0)
+        filter_med_ad(fasta_dir, stats, max_ns, c_range, s_range, m_range)
+
+    def stats_are_current():
+        genomes_in_dir = 0
+        for f in os.listdir(fasta_dir):
+            if f.endswith("fasta"):
+                genomes_in_dir += 1
+        genomes_in_stats = len(stats.index)
+        if genomes_in_dir == genomes_in_stats:
+            print("stats.csv is current. Filtering will be based on existing stats.csv")
+            return True
+        else:
+            print("stats.csv is not current.  MASH will be run and stats.csv will be updated.")
+            return False
+
+    mash_exe = "/common/contrib/bin/mash-Linux64-v1.1.1/mash"
+    fasta_dir = args.fasta_dir
+    max_ns = args.max_n_count 
+
+    if args.filter_level:
+        c_range = args.filter_level
+        s_range = args.filter_level
+        m_range = args.filter_level
+    else:
+        c_range = args.c_range
+        s_range = args.s_range
+        m_range = args.m_range
+
+    if len(os.listdir(fasta_dir)) <= 5: # pass if there are <= 5 FASTA's
+        print("{} contains less than 5 genomes.".format(fasta_dir))
+    elif os.path.isfile(os.path.join(fasta_dir, "info", "stats.csv")):
+        stats = os.path.join(fasta_dir, "info", "stats.csv")
+        stats = pd.read_csv(stats, index_col=0)
+        if stats_are_current():
+            filter_med_ad(fasta_dir, stats, max_ns, c_range, s_range, m_range)
+        else:
+            mash_stats_and_filter()
+    else:
+        mash_stats_and_filter()
+
+if __name__ == '__main__':
+    main()
 def filter_contigs(stats, passed_I, filter_ranges, c_range, failed, summary_df):
 
     contigs = passed_I["Contigs"]
@@ -344,6 +377,22 @@ def main():
             mash_stats_and_filter()
     else:
         mash_stats_and_filter()
+
+def make_passed_dir(passed_dir):
+    
+    if os.path.isdir(passed_dir):
+        rmtree(passed_dir)
+    if not os.path.isdir(passed_dir):
+        os.mkdir(passed_dir)
+
+def link_passed_genomes(species_dir, passed_dir, passed_genomes):
+
+    passed_genomes = pd.read_csv(passed_genomes, index_col=0)
+    for src in passed_genomes.index:
+        genome = "{}.fasta".format(src)
+        src = os.path.join(species_dir, genome)
+        dst = os.path.join(passed_dir, genome)
+        os.link(src, dst)
 
 if __name__ == '__main__':
     main()
