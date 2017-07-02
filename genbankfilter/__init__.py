919b847c5c4294cf78e1df14aa9d07d60ada5c14
Andrew A Sanchez <andrew@Andrews-MacBook-Pro-2.local>
2017-07-02 17:42:15 +0200

    Move functions into filter.py

--- genbankfilter/__init__.py
+++ genbankfilter/__init__.py
@@ -2,3 +2,4 @@ import genbankfilter.mash as mash
 import genbankfilter.get_resources as get_resources
 import genbankfilter.config as config
 import genbankfilter.curate as curate
+import genbankfilter.filter as filter
