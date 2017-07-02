448a9316f282c4cdccf24363c110a1715dae39bf
Andrew A Sanchez <andrew@Andrews-MacBook-Pro-2.local>
2017-07-02 17:44:30 +0200

    Improve variable names and reorganize

--- genbankfilter/mash.py
+++ genbankfilter/mash.py
@@ -5,6 +5,7 @@ import re
 import argparse
 import glob
 import subprocess
+import pandas as pd
 
 def sketch(genbank_mirror, assembly_summary, genome):
 
@@ -35,12 +36,33 @@ def paste(genbank_mirror, assembly_summary, species):
 def dist(genbank_mirror, assembly_summary, species):
 
     species_dir = os.path.join(genbank_mirror, species)
-    dst_mx = os.path.join(species_dir, 'all_dist.msh')
+    dst_mx_path = os.path.join(species_dir, 'dst_mx.txt')
     all_msh = os.path.join(species_dir,'all.msh')
-    remove_old(dst_mx)
-    dist_cmd = "mash dist -t '{}' '{}' > '{}'".format(all_msh, all_msh, dst_mx)
+    remove_old(dst_mx_path)
+    dist_cmd = "mash dist -t '{}' '{}' > '{}'".format(all_msh, all_msh, dst_mx_path)
     subprocess.Popen(dist_cmd, shell="True", stdout=subprocess.DEVNULL).wait()
+    dst_mx = format_dst_mx(dst_mx_path)
     return dst_mx
+
+def format_dst_mx(dst_mx_path):
+
+    """
+    Set indices and headers to the accession ID's
+    """
+
+    dst_mx = pd.read_csv(dst_mx_path, index_col=0, sep="\t")
+    new_index = []
+    new_columns = []
+    for i in dst_mx.index:
+        name = i.split("/")[-1].strip(".fasta")
+        new_index.append(name)
+        new_columns.append(name)
+
+    dst_mx.index = new_index
+    dst_mx.columns = new_columns
+    dst_mx.to_csv(dst_mx_path, sep="\t")
+    return dst_mx
+
 
 def remove_old(f):
     if os.path.isfile(f):
