3ab7653f657cda27bb1a16afdbfd64590032c477
Andrew A Sanchez <andrew@client701.student-vpn.nau.edu>
2017-07-03 16:37:23 +0200

    Reorganize and set species_dir to reflect value of self.species

--- genbankfilter/test/test_mash.py
+++ genbankfilter/test/test_mash.py
@@ -16,16 +16,16 @@ class TestMash(unittest.TestCase):
     def setUp(self):
         self.tmp = tempfile.mkdtemp()
         self.genbank = os.path.join(self.tmp, 'genbank')
+        shutil.copytree('genbankfilter/test/resources/',
+                        self.genbank)
         self.assembly_summary = pd.read_csv(
             'genbankfilter/test/resources/updated_assembly_summary.txt',
             sep="\t",
             index_col=0)
         self.species = 'Buchnera_aphidicola'
-        self.species_dir = os.path.join(self.tmp, 'genbank',
-                                        'Buchnera_aphidicola')
-        self.genomes = ['GCA_000007365.1', 'GCA_000007725.1']
-        shutil.copytree('genbankfilter/test/resources/', self.genbank)
-
+        self.species_dir = os.path.join(self.tmp,
+                                        'genbank',
+                                        self.species)
         self.genomes = [re.match('GCA_\d+\.\d', f).group() for f in os.listdir(self.species_dir)]
         for genome in self.genomes:
             mash.sketch(self.genbank, self.assembly_summary, genome)
