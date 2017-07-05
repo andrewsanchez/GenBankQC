88d77563ea1b7e70deb4477b0d19048e2a0610f0
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-07-05 17:59:29 +0200

    Update tests to reflect how I plan to call functions in __main__

--- genbankfilter/test/test_mash.py
+++ genbankfilter/test/test_mash.py
@@ -26,29 +26,46 @@ class TestMash(unittest.TestCase):
         self.species_dir = os.path.join(self.genbank, self.species)
         self.genomes = mash.find_all_genome_paths(self.genbank)
 
+    def test_find_all_genome_paths(self):
         for genome in self.genomes:
-            mash.sketch(self.genbank, self.assembly_summary, genome)
-        self.all_msh = mash.paste(self.genbank, self.assembly_summary, self.species)
-        self.dst_mx = mash.dist(self.genbank,
-                                self.assembly_summary,
-                                self.species)
-        self.stats = filter.generate_stats(self.species_dir, self.dst_mx)
-
-    def test_mash_and_stats(self):
+            self.assertTrue(os.path.isfile(genome))
+        
+    def test_sketch_genome(self):
         for genome in self.genomes:
-            sketch = os.path.join(self.species_dir, '{}.msh'.format(genome))
+            mash.sketch_genome(genome)
+        sketches = mash.find_all_sketches(self.species_dir)
+        self.assertTrue(len(sketches) == len(self.genomes))
+        for sketch in sketches:
             self.assertTrue(os.path.isfile(sketch))
-        self.assertTrue(os.path.isfile(self.all_msh))
-        self.assertIsInstance(self.dst_mx, pd.DataFrame)
-        self.assertIsInstance(self.stats, pd.DataFrame)
+        
+    def test_sketch_species_dir(self):
+        mash.sketch_dir(self.species_dir) 
+        sketches = mash.find_all_sketches(self.species_dir)
+        self.assertTrue(len(sketches) == len(self.genomes))
+        for sketch in sketches:
+            self.assertTrue(os.path.isfile(sketch))
 
-    def test_filter_and_link(self):
-        filter_summary, failed, passed_final = filter.filter_med_ad(self.species_dir, self.stats)
+    def test_sketch_genbank(self):
+        mash.sketch_dir(self.genbank) 
+        sketches = mash.find_all_sketches(self.genbank)
+        self.assertTrue(len(sketches) == len(self.genomes))
+        for sketch in sketches:
+            self.assertTrue(os.path.isfile(sketch))
+       
+    def test_paste_dist_stats(self):
+        mash.sketch_dir(self.genbank) 
+        all_msh = mash.paste(self.species_dir)
+        dst_mx = mash.dist(self.species_dir)
+        stats = filter.generate_stats(self.species_dir, dst_mx)
+        filter_summary, failed, passed_final = filter.filter_med_ad(self.species_dir, stats)
         passed_dir = filter.check_passed_dir(self.species_dir)
         filter.link_passed_genomes(self.species_dir, passed_final, passed_dir)
+        self.assertTrue(os.path.isfile(all_msh))
+        self.assertIsInstance(dst_mx, pd.DataFrame)
+        self.assertIsInstance(stats, pd.DataFrame)
         self.assertTrue(passed_dir)
         self.assertIsInstance(filter_summary, pd.DataFrame)
-    
+
     def tearDown(self):
         shutil.rmtree(self.genbank)
 
