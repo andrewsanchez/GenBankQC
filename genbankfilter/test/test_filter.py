c4b2b59da9416f830f89e12d6402ae7305644fe1
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-08-23 16:37:32 +0200

    Test stats function

--- genbankfilter/test/test_filter.py
+++ genbankfilter/test/test_filter.py
@@ -1,22 +1,28 @@
 import unittest
-import os
 import genbankfilter.filter as gbf
 
 
 class TestFilter(unittest.TestCase):
     def setUp(self):
         self.species_dir = "genbankfilter/test/resources/Buchnera_aphidicola"
-        self.fastas = (f for f in os.listdir(self.species_dir))
+        self.fastas = gbf.get_all_fastas(self.species_dir)
 
-    def test_stats_function(self):
+    def test_stats_functions(self):
         from Bio.Seq import Seq
-        fasta = os.path.join(self.species_dir, next(self.fastas))
-        contigs, contig_count = gbf.get_contigs(fasta)
+        fasta = next(self.fastas)
+        contigs, contig_count = gbf.get_contigs(fasta, [])
         self.assertTrue(type(contigs[0]) == Seq)
         self.assertTrue(type(contig_count) == int)
-        assembly_size = gbf.get_assembly_size(contigs)
+        assembly_size = gbf.get_assembly_size(contigs, [])
         self.assertTrue(type(assembly_size) == int)
         self.assertTrue(type(assembly_size) != 0)
+        N_count = gbf.get_N_count(contigs, [])
+        self.assertTrue(type(N_count == int))
+
+    def test_generate_stats(self):
+        dmx = gbf.read_dmx(self.species_dir)
+        stats = gbf.generate_stats(self.species_dir, dmx)
+        self.assertTrue(type(stats) == gbf.pd.DataFrame)
 
 
 if __name__ == '__main__':
