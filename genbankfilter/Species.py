13ce2395491b1f98cdebcbf2becd9e051f4a8d8b
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-09-04 22:20:20 +0200

    Simplify Species class

--- genbankfilter/Species.py
+++ genbankfilter/Species.py
@@ -19,15 +19,7 @@ class Species:
             The path to the directory of related genomes you wish to analyze.
     """
 
-    color_map = {
-        "N_Count": "red",
-        "Contigs": "green",
-        "MASH": "blue",
-        "Assembly_Size": "purple"
-    }
-
-    def __init__(self, species_dir, max_n_count=200, c_range=3.0, s_range=3.0,
-                 m_range=3.0):
+    def __init__(self, species_dir):
         self.species_dir = species_dir
         stats = os.path.join(self.species_dir, 'stats.csv')
         if os.path.isfile(stats):
@@ -35,9 +27,3 @@ class Species:
         nw_file = os.path.join(species_dir, 'tree.nw')
         if os.path.isfile(nw_file):
             self.tree = Tree(nw_file, 1)
-        self.max_n_count = max_n_count
-        self.c_range = c_range
-        self.s_range = s_range
-        self.m_range = m_range
-        # probably won't need this
-        self.filter_ranges = [max_n_count, c_range, s_range, m_range]
