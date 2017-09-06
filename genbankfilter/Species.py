531b1dd67e904facf7a519a478d7e587e10f4ac1
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-09-06 16:14:28 +0200

    Remove unnecessary imports

--- genbankfilter/Species.py
+++ genbankfilter/Species.py
@@ -1,15 +1,6 @@
 import os
-import re
-import shutil
 import pandas as pd
-import numpy as np
 from ete3 import Tree
-from Bio import SeqIO
-from Bio import Phylo
-from Bio.Phylo.TreeConstruction import _DistanceMatrix
-from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
-
-from collections import namedtuple
 
 
 class Species:
