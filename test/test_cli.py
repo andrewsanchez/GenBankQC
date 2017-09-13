8f22b009ae2927383798be749adf766b61170f48
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-09-13 15:24:14 +0200

    Update command line tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,29 +2,31 @@ import unittest
 import tempfile
 import shutil
 import os
-import click
 from click.testing import CliRunner
 
 from genbankfilter.__main__ import cli
+
 
 class TestCli(unittest.TestCase):
     def setUp(self):
         self.runner = CliRunner()
         self.tmp = tempfile.mkdtemp()
         self.genbank = os.path.join(self.tmp, 'genbank')
-        shutil.copytree('genbankfilter/test/resources/',
+        shutil.copytree('test/resources/',
                         self.genbank)
         self.species = 'Buchnera_aphidicola'
         self.species_dir = os.path.join(self.genbank, self.species)
 
     def test_levels(self):
-        level = 2.0
-        result = self.runner.invoke(cli, ['--filter-level', level, self.species_dir])
+        result = self.runner.invoke(cli, ['-n', 1000, '-m', 3, '-s', 1, '-c',
+                                          2.0, '-d', self.species_dir])
+        print(result.output)
         print(result.exit_code)
         self.assertEqual(result.exit_code, 0)
-        self.assertTrue('Contigs:  {}'.format(level) in result.output)
-        self.assertTrue('Assembly size:  {}'.format(level) in result.output)
-        self.assertTrue('MASH distances:  {}'.format(level) in result.output)
+        self.assertIn(str(1000), result.output)
+        self.assertIn(str(3.0), result.output)
+        self.assertIn(str(1.0), result.output)
+        self.assertIn(str(2.0), result.output)
 
     def tearDown(self):
         shutil.rmtree(self.tmp)
