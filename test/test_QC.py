cf3f3865f95f7eb39a2665c70dcd024aef849512
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-10-03 17:51:41 +0200

    Remove unused imports and old unittest stuff

--- test/test_QC.py
+++ test/test_QC.py
@@ -1,7 +1,4 @@
 import os
-import shutil
-import tempfile
-import unittest
 
 from pandas import DataFrame, Index
 
@@ -57,35 +54,3 @@ def test_filter_med_abs_dev(species):
         assert passed_and_failed == genomes_before_filtering
 
 
-class TestFilteredSpecies(unittest.TestCase):
-
-    def setUp(self):
-        self.tmp = tempfile.mkdtemp()
-        self.genbank = os.path.join(self.tmp, 'genbank')
-        shutil.copytree('test/resources/', self.genbank)
-        self.species = 'Buchnera_aphidicola'
-        self.path = os.path.join(self.genbank, self.species)
-        self.B_aphidicola = gbf.FilteredSpecies(self.path)
-
-    def test_str(self):
-        print(self.B_aphidicola)
-
-    def test_summary(self):
-        pass
-
-    def test_filter_all(self):
-        import subprocess
-        species_dir = os.path.join(self.genbank, "Acinetobacter_baumannii")
-        baumannii = gbf.FilteredSpecies(species_dir)
-        gbf.filter_all(baumannii)
-        tree_svg = os.path.join(baumannii.species_dir, "tree_200-3.0-3.0-3.0.svg")
-        home = subprocess.Popen(
-            "echo $HOME",
-            shell=True,
-            stdout=subprocess.PIPE).communicate()[0].decode().strip()
-        shutil.move(tree_svg, "{}/scratch/test_tree.svg".format(home))
-        # tree_svg = "/Users/andrew/scratch/test_tree.svg"
-        # subprocess.Popen("open {}".format(tree_svg), shell=True)
-
-    def tearDown(self):
-        shutil.rmtree(self.genbank)
