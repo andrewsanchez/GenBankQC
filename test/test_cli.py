e0fcd78d6d2fce9607a9ed7203d209605580c0ce
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-10-05 16:53:25 +0200

    Clean up test_cli

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,17 +1,9 @@
-import os
-import shutil
-import tempfile
-import unittest
-
 from click.testing import CliRunner
 
 from genbank_qc.__main__ import cli
 
 
-def test_cli(aphidicola_bare):
-    aphidicola = aphidicola_bare
+def test_cli(path):
     runner = CliRunner()
-    result = runner.invoke(cli, [aphidicola.path])
-    print(result.output)
-    print(aphidicola.path)
+    result = runner.invoke(cli, [path])
     assert result.exit_code == 0
