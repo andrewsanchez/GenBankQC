6b6ffe2eebe067078cf89e3f92876001236e9404
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-12-09 17:06:40 -0700

    Rename test_cli to test___main__ for quick toggling with projectile

---
+++ test/test___main__.py
@@ -0,0 +1,17 @@
+from click.testing import CliRunner
+
+from genbankqc.__main__ import cli
+
+
+def test_cli(aphidicola_bare):
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--subdir', aphidicola_bare.path])
+    assert result.exit_code == 0
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--help'])
+    print(result.output)
+    assert result.exit_code == 0
+
