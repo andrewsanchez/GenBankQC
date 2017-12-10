8e4b60d60d12698c91965967be4cdfa1724e71b5
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-12-09 17:09:05 -0700

    Better help text and option/arg names

--- genbankqc/__main__.py
+++ genbankqc/__main__.py
@@ -2,27 +2,34 @@ import click
 
 from genbankqc import Species, Genbank
 
+help_text = """
+Assess the integrity of your FASTA collection.
 
-@click.command()
-@click.option('-l', '--filter-level', type=float,
-              help='Value to be used for all filters')
+Run genbankqc on subdirectories in parent directory GENKBANK.
+Subdirectories should contain 5 or more FASTAs.
+"""
+
+
+@click.command(help=help_text)
 @click.option('-n', '--max_unknowns', type=int, default=200,
-              help='Maximum number of acceptable unknown bases')
-@click.option('-c', '--c-tolerance', type=float, default=3.0,
-              help='Tolerance level for number of contigs',)
-@click.option('-s', '--s-tolerance', type=float, default=3.0,
-              help='Tolerance level for the assembly size')
-@click.option('-m', '--m-tolerance', type=float, default=3.0,
-              help='Tolerance level for MASH distances')
+              help='Maximum number of unknown bases')
+@click.option('-c', '--c-deviations', type=float, default=3.0,
+              help='Deviations for number of contigs',)
+@click.option('-s', '--s-deviations', type=float, default=3.0,
+              help='Deviations for the assembly size')
+@click.option('-m', '--m-deviations', type=float, default=3.0,
+              help='Deviations for MASH distances')
+@click.option('-l', '--filter-level', type=float,
+              help='Deviations for all metrics')
 @click.option('-d', '--dry-run', is_flag=True)
-@click.option('--subdir', is_flag=True)
-@click.argument('path', type=click.Path(exists=True, file_okay=False))
-def cli(filter_level, max_unknowns, c_tolerance, s_tolerance, m_tolerance,
-        dry_run, subdir, path):
-    """Assess the integrity of your FASTA collection."""
+@click.option('--subdir', is_flag=True,
+              help='Run on single species')
+@click.argument('genbank', type=click.Path(exists=True, file_okay=False))
+def cli(filter_level, max_unknowns, c_deviations, s_deviations, m_deviations,
+        dry_run, subdir, genbank):
     if subdir:
-        species = Species(path, max_unknowns, c_tolerance, s_tolerance,
-                          m_tolerance)
+        species = Species(genbank, max_unknowns, c_deviations, s_deviations,
+                          m_deviations)
         species.qc()
     else:
-        Genbank(path).qc()
+        Genbank(genbank).qc()
