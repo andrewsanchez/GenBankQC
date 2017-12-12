877655a32d6a35f2c4fe31bf17cab7ad4b69252d
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-12-12 15:08:12 -0700

    Version 0.2a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='GenBankQC',
-      version='0.1a',
+      version='0.2a',
       license='BSD-3-Clause',
       keywords='NCBI bioinformatics genomics',
       packages=find_packages(),
