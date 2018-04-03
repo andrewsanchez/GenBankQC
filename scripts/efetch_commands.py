e0a3dfaf0dc317c1cdc53839f623305215f1f6ec
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-04-02 17:13:16 -0700

    Scripts for downloading and parsing biosample data

---
+++ scripts/efetch_commands.py
@@ -0,0 +1,31 @@
+import subprocess
+import os, stat
+import shlex
+
+
+bacteria_genbank = "/common/contrib/databases/bacteria_genbank"
+d = os.path.join(bacteria_genbank, "biosample_data")
+if not os.path.isdir(d):
+    os.mkdir(d)
+
+
+with open("/tmp/biosample_ids.txt") as f:
+    ids = (i.strip() for i in f.readlines())
+
+cmds = os.path.join(d, "efetch.sh")
+if os.path.isfile(cmds):
+    os.remove(cmds)
+
+with open(cmds, "a") as f:
+    ext = ".xml"
+    for i in ids:
+        o = os.path.join(d, i + ext)
+        if os.path.isfile(o):
+            continue
+        cmd = (
+            "esearch -db biosample -query {} | "
+            "efetch -format docsum "
+            "> {}\n".format(i, o)
+        )
+        f.write(cmd)
+os.chmod(cmds, stat.S_IRWXU)
---
+++ scripts/parse_biosamples.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+import xml.etree.cElementTree as ET
+from  xml.etree.ElementTree import ParseError
+from pprint import pprint as pp
+
+fields = [
+    "geo_loc_name",
+    "collection_date",
+    "strain",
+    "isolation_source",
+    "host",
+    "collected_by",
+    "sample_type",
+    "sample_name",
+    "host_disease",
+    "isolate",
+    "host_health_state",
+    "serovar",
+    "env_biome",
+    "env_feature",
+    "ref_biomaterial",
+    "env_material",
+    "isol_growth_condt",
+    "num_replicons",
+    "sub_species",
+    "host_age",
+    "genotype",
+    "host_sex",
+    "serotype",
+    "host_disease_outcome",
+]
+
+biosamples = "/mnt/data0/home/asanchez/scratch/efetch_results"
+fs = (os.path.join(biosamples, f) for f in os.listdir(biosamples))
+for f in fs:
+    df = pd.DataFrame()
+    out = os.path.splitext(os.path.basename(f))[0] + '.csv'
+    try:
+        tree = ET.ElementTree(file=f)
+    except ParseError:
+        continue
+    accession = tree.find("DocumentSummary/Accession").text
+    xp = 'DocumentSummary/SampleData/BioSample/Attributes/Attribute/[@harmonized_name]'
+    attribs = tree.iterfind(xp)
+    for i in attribs:
+        name = i.attrib["harmonized_name"]
+        df.loc[accession, name] = i.text
+    print(df)
+    df.to_csv(out)
