40d605c4a408b184570529efe17160e11fc907b8
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-06-20 14:44:17 -0700

    import class instead of module

--- genbankqc/__init__.py
+++ genbankqc/__init__.py
@@ -1,6 +1,6 @@
 from .Genome import Genome
 from .Species import Species
 from .Genbank import Genbank
-from . import Metadata
+from .Metadata import Metadata
 
 __all__ = [Genome, Species, Genbank, Metadata]
