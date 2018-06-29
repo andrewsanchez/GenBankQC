bf0b1557ff9f8ed39b7dbcabb9b24fa4a548cf22
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-06-29 15:19:28 -0700

    Remove this unused property

--- genbankqc/Genome.py
+++ genbankqc/Genome.py
@@ -49,41 +49,42 @@ class Genome:
             self.stats_df = None
         # TODO: Maybe include the species_mean_distance here
 
-    @property
-    def ids(self):
-        ids = {
-            "accession": self.accession_id,
-            "biosample": self.biosample_id,
-            "sra": self.sra,
-            "srs": self.srs,
-        }
-        return ids
-
     @retry(stop_max_attempt_number=7, stop_max_delay=10000, wait_fixed=2000)
     def get_biosample(self):
         # TODO save file object in memory instead of saving to disk
         cmd = (
             "esearch -db biosample -query {} | "
             "efetch -format docsum "
-            "> {}\n".format(self.biosample_id, self.biosample_xml)
+            "> {}\n".format(self.metadata["biosample_id"], self.biosample_xml)
         )
         if not os.path.isfile(self.biosample_xml):
-            subprocess.Popen(cmd, shell="True", stderr=subprocess.DEVNULL).wait()
+            subprocess.Popen(
+                cmd, shell="True", stderr=subprocess.DEVNULL).wait()
 
     def parse_biosample(self):
         # TODO Parse file object from get_biosample() in memory
         try:
-            tree = ET.ElementTree(self.biosample_xml)
+            tree = ET.ElementTree(file=self.biosample_xml)
         except ParseError:
             # log
             pass
-        try:
-            sra_path = ('DocumentSummary/SampleData/'
+        sra = tree.find('DocumentSummary/SampleData/'
                         'BioSample/Ids/Id/[@db="SRA"]')
-            self.sra = tree.find(sra_path).text
+        try:
+            self.metdata["sra"] = sra.text
         except AttributeError:
-            # log
-            self.sra = None
+            pass
+        for name in Metadata.biosample_fields:
+            xp = ('DocumentSummary/SampleData/BioSample/Attributes/Attribute'
+                  '[@harmonized_name="{}"]'.format(name))
+            attrib = tree.find(xp)
+            try:
+                self.metadata[name] = attrib.text
+            except AttributeError:
+                pass
+
+    def get_sra(self):
+        pass
 
     def get_contigs(self):
         """Return a list of of Bio.Seq.Seq objects for fasta and calculate
