bcc189f4218fe5e320c71892deeb76632e67e88b
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-07-04 17:20:55 -0700

    Create a dict to hold xml

--- genbankqc/Genome.py
+++ genbankqc/Genome.py
@@ -41,6 +41,7 @@ class Genome:
                 self.accession_id].biosample
             self.biosample_xml = os.path.join(
                 self.qc_dir, self.metadata["biosample_id"] + ".xml")
+        self.xml = {}
         self.msh = os.path.join(self.qc_dir, self.name + ".msh")
         self.stats_path = os.path.join(self.qc_dir, self.name + '.csv')
         if os.path.isfile(self.stats_path):
