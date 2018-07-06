ca1917846240a5139559eb4dd14bfd763d7fc0a7
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-07-06 13:41:04 -0700

    Rearrange order of class methods

--- genbankqc/Genome.py
+++ genbankqc/Genome.py
@@ -48,6 +48,51 @@ class Genome:
             self.stats_df = None
         # TODO: Maybe include the species_mean_distance here
 
+    def get_contigs(self):
+        """Return a list of of Bio.Seq.Seq objects for fasta and calculate
+        the total the number of contigs.
+        """
+        try:
+            self.contigs = [seq.seq for seq in SeqIO.parse(self.path, "fasta")]
+            self.count_contigs = len(self.contigs)
+        except UnicodeDecodeError:
+            self.contigs = UnicodeDecodeError
+
+    def get_assembly_size(self):
+        """Calculate the sum of all contig lengths"""
+        # TODO: map or reduce might be more elegant here
+        self.assembly_size = sum((len(str(seq)) for seq in self.contigs))
+
+    def get_unknowns(self):
+        """Count the number of unknown bases, i.e. not [ATCG]"""
+        # TODO: Would it be useful to allow the user to define p?
+        p = re.compile("[^ATCG]")
+        self.unknowns = sum((len(re.findall(p, str(seq)))
+                             for seq in self.contigs))
+
+    def get_distance(self, dmx_mean):
+        self.distance = dmx_mean.loc[self.name]
+
+    def sketch(self):
+        cmd = "mash sketch '{}' -o '{}'".format(self.path, self.msh)
+        if not os.path.isfile(self.msh):
+            subprocess.Popen(
+                cmd, shell="True", stderr=subprocess.DEVNULL).wait()
+
+    def get_stats(self, dmx_mean):
+        from pandas import DataFrame
+        if not os.path.isfile(self.stats_path):
+            self.get_contigs()
+            self.get_assembly_size()
+            self.get_unknowns()
+            self.get_distance(dmx_mean)
+            data = {"contigs": self.count_contigs,
+                    "assembly_size": self.assembly_size,
+                    "unknowns": self.unknowns,
+                    "distance": self.distance}
+            self.stats = DataFrame(data, index=[self.name])
+            self.stats.to_csv(self.stats_path)
+
     @retry(stop_max_attempt_number=7, stop_max_delay=10000, wait_fixed=2000)
     def efetch(self, db):
         # TODO: Figure out how to retry if the process doesn't complete
@@ -104,48 +149,3 @@ class Genome:
             self.metadata["srs_accessions"] = ','.join(srs_accessions)
         except ParseError:
             self.metadata["srs_accessions"] = "missing"
-
-    def get_contigs(self):
-        """Return a list of of Bio.Seq.Seq objects for fasta and calculate
-        the total the number of contigs.
-        """
-        try:
-            self.contigs = [seq.seq for seq in SeqIO.parse(self.path, "fasta")]
-            self.count_contigs = len(self.contigs)
-        except UnicodeDecodeError:
-            self.contigs = UnicodeDecodeError
-
-    def get_assembly_size(self):
-        """Calculate the sum of all contig lengths"""
-        # TODO: map or reduce might be more elegant here
-        self.assembly_size = sum((len(str(seq)) for seq in self.contigs))
-
-    def get_unknowns(self):
-        """Count the number of unknown bases, i.e. not [ATCG]"""
-        # TODO: Would it be useful to allow the user to define p?
-        p = re.compile("[^ATCG]")
-        self.unknowns = sum((len(re.findall(p, str(seq)))
-                             for seq in self.contigs))
-
-    def get_distance(self, dmx_mean):
-        self.distance = dmx_mean.loc[self.name]
-
-    def sketch(self):
-        cmd = "mash sketch '{}' -o '{}'".format(self.path, self.msh)
-        if not os.path.isfile(self.msh):
-            subprocess.Popen(
-                cmd, shell="True", stderr=subprocess.DEVNULL).wait()
-
-    def get_stats(self, dmx_mean):
-        from pandas import DataFrame
-        if not os.path.isfile(self.stats_path):
-            self.get_contigs()
-            self.get_assembly_size()
-            self.get_unknowns()
-            self.get_distance(dmx_mean)
-            data = {"contigs": self.count_contigs,
-                    "assembly_size": self.assembly_size,
-                    "unknowns": self.unknowns,
-                    "distance": self.distance}
-            self.stats = DataFrame(data, index=[self.name])
-            self.stats.to_csv(self.stats_path)
