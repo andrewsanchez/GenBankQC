9acf7ea13920fcc4b3725ef0d45157c226faf7e5
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-07-27 18:18:48 -0700

    rm deprecated scripts folder

--- scripts/get_biosample_ids.sh
+++
@@ -1,6 +0,0 @@
-# get list of biosample ids and generate edirect commands
-summary=/common/contrib/databases/bacteria_genbank/.info/assembly_summary.txt
-cut -f 3 "$summary" | sed '1d' > /tmp/biosample_ids.txt
-python efetch_commands.py
-chmod 755 /tmp/efetch.sh
-# bash /tmp/efetch.sh
