1a6509ea8eb6006e3c9b356fcc211c5319cb862e
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-08-23 13:55:41 -0700

    Clean up GenBank

--- genbankqc/Genbank.py
+++ genbankqc/Genbank.py
@@ -8,48 +8,30 @@ from genbankqc import Species
 
 class Genbank:
     def __init__(self, path):
-
-        """Genbank"""
-
+        """
+        GenBank
+        """
         self.genbank = path
         self.path = path
-        self.assembly_summary = os.path.join(
-            self.genbank,
-            ".info/assembly_summary.txt",
-        )
+        self.assembly_summary = os.path.join(self.genbank,
+                                             ".info/assembly_summary.txt")
         try:
-            self.assembly_summary = pd.read_csv(
-                self.assembly_summary,
-                sep="\t",
-                index_col=0,
-            )
+            self.assembly_summary = pd.read_csv(self.assembly_summary,
+                                                sep="\t", index_col=0)
         except FileNotFoundError:
-            pass
-        self.log = Logger("init.genbank")
-        self.log.info(self.genbank)
+            self.log.error("No assembly summary")
+        self.log = Logger("GenBank")
+        self.log.info("Instantiated")
 
     @property
     def species(self):
-        dirs = (
-            os.path.join(self.genbank, d)
-            for d in os.listdir(self.genbank)
-            if os.path.isdir(d)
-        )
-        for d in dirs:
-            fastas = len([f for f in os.listdir(d) if f.endswith('fasta')])
-            if fastas > 5:
-                try:
-                    yield Species(d)
-                except Exception:
-                    print('Skipping ', d)
-                    traceback.print_exc()
-            else:
-                print("Not enough fastas in {}".format(d))
+        for d in os.path.listdir(self.genbank):
+            if d.startswith('.'):
+                continue
+            path = os.path.join(self.genbank, d)
+            if os.path.isdir(path):
+                yield Species(path, self.assembly_summary)
 
     def qc(self):
         for i in self.species:
-            try:
-                i.qc()
-            except Exception:
-                print('Failed ', i.species)
-                traceback.print_exc()
+            i.qc()
