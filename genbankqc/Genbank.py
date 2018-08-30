9f9a7eb9ec1ed217093d6bbf857f494c84fd5a15
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-08-29 18:07:43 -0700

    Formatting

--- genbankqc/Genbank.py
+++ genbankqc/Genbank.py
@@ -4,6 +4,9 @@ from logbook import Logger
 
 from genbankqc import Species
 
+taxdump_url = "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz"
+assembly_summary_url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt"
+
 
 class Genbank:
     def __init__(self, path):
@@ -11,13 +14,12 @@ class Genbank:
         GenBank
         """
         self.path = path
-        self.assembly_summary = os.path.join(self.path,
-                                             ".info/assembly_summary.txt")
+        self.assembly_summary_path = os.path.join(self.path, ".info/assembly_summary.txt")
         try:
-            self.assembly_summary = pd.read_csv(self.assembly_summary,
-                                                sep="\t", index_col=0)
+            self.assembly_summary = pd.read_csv(self.assembly_summary, sep="\t", index_col=0)
         except FileNotFoundError:
-            self.log.error("No assembly summary")
+            self.assembly_summary = pd.read_csv(assembly_summary_url, sep="\t", index_col=0, skiprows=1)
+            self.assembly_summary.to_csv(self.assembly_summary_path, sep="\t")
         self.log = Logger("GenBank")
         self.log.info("Instantiated")
 
@@ -28,8 +30,7 @@ class Genbank:
                 continue
             path = os.path.join(self.path, d)
             if os.path.isdir(path):
-                yield Species.Species(path,
-                                      assembly_summary=self.assembly_summary)
+                yield Species.Species(path, assembly_summary=self.assembly_summary)
 
     def qc(self):
         for i in self.species:
