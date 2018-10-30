5ad598690988abad2b71e7504b351d903a9b5554
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-10-30 16:29:24 -0700

    Move QT configuration into __init__.py

--- genbankqc/__init__.py
+++ genbankqc/__init__.py
@@ -1,7 +1,13 @@
+import os
+
 from .genome import Genome
 from .species import Species
 from .genbank import Genbank
 from .metadata import Metadata
 from .metadata import BioSample
+
+
+# Figure out how to supress error output from this
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 __all__ = [Genome, Species, Genbank, Metadata]
