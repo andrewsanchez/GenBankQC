bc261571624100d000255eebbecf37389a7a00c1
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-10-30 16:28:52 -0700

    Import BioSample

--- genbankqc/__init__.py
+++ genbankqc/__init__.py
@@ -2,5 +2,6 @@ from .genome import Genome
 from .species import Species
 from .genbank import Genbank
 from .metadata import Metadata
+from .metadata import BioSample
 
 __all__ = [Genome, Species, Genbank, Metadata]
