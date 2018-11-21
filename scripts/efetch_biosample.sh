45241e7fc4e59a9092551e6b3aadc550158be0f8
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-21 13:35:00 -0700

    Add notes to script and add fetching run IDs

--- scripts/efetch_biosample.sh
+++ scripts/efetch_biosample.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-genbank=$1
-biosample_xml="$genbank/metadata/biosample.xml"
-biosample_xtract="$genbank/metadata/biosample.txt"
+dir=$1
+biosample_xml="$dir/biosample.xml"
+biosample_xtract="$dir/biosample.txt"
 
-mkdir "$genbank/metadata"
-esearch -db biosample  -query 'bacteria[orgn] AND biosample_assembly[filter]' | \
-    efetch -format docsum > "$biosample_xml"
+# This data is retrieved using Biopython
+# esearch -db biosample  -query 'bacteria[orgn] AND biosample_assembly[filter]' | \
+#     efetch -format docsum > "$biosample_xml"
 
-xtract -input "$biosample_xml" -pattern DocumentSummary/* \
-    -group SampleData/BioSample \
-        -if Attribute@harmonized_name \
-            -lbl "accession" -element @accession \
-    -group Attribute  \
-        -if Attribute@harmonized_name \
-            -def "missing" -element @harmonized_name Attribute > "$biosample_xtract"
+# This data is retrieved using Biopython
+# xtract -input "$biosample_xml" -pattern DocumentSummary/* \
+#     -group SampleData/BioSample \
+#         -if Attribute@harmonized_name \
+#             -lbl "accession" -element @accession \
+#     -group Attribute  \
+#         -if Attribute@harmonized_name \
+#             -def "missing" -element @harmonized_name Attribute > "$biosample_xtract"
+
+# Get SRA runs for each ID
+for f in SRA*txt;
+do epost -db sra -input $f -format acc | \
+        efetch -format docsum | \
+        xtract -pattern DocumentSummary/* \
+               -group Biosample -ret '\t' -element Biosample \
+               -group Runs -sep ',' -element Runs/Run@acc \
+               >> SRA_runs.txt
+done
