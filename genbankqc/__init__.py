ec5b9826422e567879dcc31b14555bfec92a4d7b
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-21 13:26:52 -0700

    Add path factory

--- genbankqc/__init__.py
+++ genbankqc/__init__.py
@@ -1,4 +1,24 @@
 import os
+import attr
+
+@attr.s
+class Paths(object):
+    subdirs = attr.ib()
+    root = attr.ib(default=os.getcwd())
+    def __attrs_post_init__(self):
+        for name in self.subdirs:
+            path = os.path.join(self.root, name)
+            object.__setattr__(self, name, path)
+
+    def mkdirs(self):
+        """Create `root` and `subdirs` if they don't already exist."""
+        if not os.path.isdir(self.root):
+            os.mkdir(self.root)
+        for subdir in self.subdirs:
+            path = self.__getattribute__(subdir)
+            if not os.path.isdir(path):
+                os.mkdir(path)
+
 
 from .genome import Genome
 from .species import Species
