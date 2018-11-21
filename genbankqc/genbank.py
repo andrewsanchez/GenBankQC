3c80b0a3426dec6b6c3d2efe3cda4698905d3d4d
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-21 14:06:37 -0700

    Update imports

--- genbankqc/genbank.py
+++ genbankqc/genbank.py
@@ -1,9 +1,10 @@
 import os
+import attr
 import pandas as pd
 from logbook import Logger
 
+from genbankqc import Paths
 from genbankqc import Species
-from genbankqc.metadata import Metadata
 
 taxdump_url = "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz"
 assembly_summary_url = "ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt"
