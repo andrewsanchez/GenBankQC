1092f68f5d2a4fa7869494781713a75514909c0b
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-26 14:08:10 -0700

    Test assembly summary methods

--- test/metadata_test.py
+++ test/metadata_test.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from pathlib import Path
 
@@ -5,12 +6,15 @@ import pandas as pd
 from genbankqc import metadata
 
 
+assembly_summary = Path("test/resources/metadata/assembly_summary.txt")
+
+
 def test_existing_assembly_summary():
-    path = Path().cwd() / "resources" / ".info" / "assembly_summary.txt"
-    summary = metadata.AssemblySummary(path)
+    summary = metadata.AssemblySummary(assembly_summary, latest=False)
     assert isinstance(summary.df, pd.DataFrame)
 
 
 def test_download_assembly_summary():
     summary = metadata.AssemblySummary(tempfile.mktemp())
+    assert os.path.isfile(summary.path)
     assert isinstance(summary.df, pd.DataFrame)
