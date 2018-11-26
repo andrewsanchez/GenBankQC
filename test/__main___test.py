56cc479501b8d9de38069cad4af82370fc299688
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-26 14:06:33 -0700

    Fix cli tests

--- test/__main___test.py
+++ test/__main___test.py
@@ -17,12 +17,12 @@ def test_help():
 
 def test_species(genbank, aphidicola):
     runner = CliRunner()
-    result = runner.invoke(cli, [genbank.path, 'species', aphidicola.path])
+    result = runner.invoke(cli, [genbank.root, 'species', aphidicola.path])
     assert result.exit_code == 0
 
 
 def test_genome(genbank, genome):
     genome, handler = genome
     runner = CliRunner()
-    result = runner.invoke(cli, [genbank.path, 'genome', genome.path])
+    result = runner.invoke(cli, [genbank.root, 'genome', genome.path])
     assert result.exit_code == 0
