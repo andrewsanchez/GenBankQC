6964d15a3a6a894215385a9d116c36d41de2bc3a
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-26 14:06:15 -0700

    Clean up __init__

--- genbankqc/__init__.py
+++ genbankqc/__init__.py
@@ -1,13 +1,25 @@
 import os
+from pathlib import Path
+
 import attr
+
+from . import metadata as metadata
+from .genbank import Genbank
+from .genome import Genome
+from .metadata import BioSample
+from .species import Species
+
+__all__ = [Genome, Species, Genbank, BioSample]
+
 
 @attr.s
 class Paths(object):
     subdirs = attr.ib()
-    root = attr.ib(default=os.getcwd())
+    root = attr.ib(default=Path.cwd())
+
     def __attrs_post_init__(self):
         for name in self.subdirs:
-            path = os.path.join(self.root, name)
+            path = self.root / name
             object.__setattr__(self, name, path)
 
     def mkdirs(self):
@@ -20,13 +32,5 @@ class Paths(object):
                 os.mkdir(path)
 
 
-from .species import Species
-from .genbank import Genbank
-from .metadata import BioSample
-from .genome import Genome
-
-
 # Figure out how to supress error output from this
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
-
-__all__ = [Genome, Species, Genbank, BioSample]
