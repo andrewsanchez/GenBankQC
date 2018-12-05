97415efca5a3bfcff81853649a72afdbc89e77a0
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-12-05 15:29:28 -0700

    Handle strings or Path objects for root attr.ib

--- genbankqc/config.py
+++ genbankqc/config.py
@@ -5,11 +5,11 @@ from pathlib import Path
 @attr.s
 class Paths(object):
     subdirs = attr.ib()
-    root = attr.ib(default=Path.cwd(), validator=attr.validators.instance_of(Path))
+    root = attr.ib(default=Path.cwd())
 
     def __attrs_post_init__(self):
-        for name in self.subdirs:
-            path = self.root / name
+        if not isinstance(self.root, Path):
+            self.root = Path(self.root)
             object.__setattr__(self, name, path)
         self.mkdirs()
 
