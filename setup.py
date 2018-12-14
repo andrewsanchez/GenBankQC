958c2965281ef4fa1f0ebd2dc5d2a75f3cfc9269
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-12-13 18:14:32 -0700

    Replace retyring with tenacity

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup, find_packages
 
 
-setup(name='GenBankQC',
-      version='0.2a',
-      license='BSD-3-Clause',
-      keywords='NCBI bioinformatics genomics',
-      packages=find_packages(),
-      python_requires='>=3.4',
-      install_requires=[
-          'click',
-          'pandas',
-          'pytest',
-          'biopython',
-          'retrying',
-          'logbook',
-      ],
-      entry_points='''
+setup(
+    name="GenBankQC",
+    version="0.2a",
+    license="BSD-3-Clause",
+    keywords="NCBI bioinformatics genomics",
+    packages=find_packages(),
+    python_requires=">=3.4",
+    install_requires=["click", "pandas", "pytest", "biopython", "tenacity", "logbook"],
+    entry_points="""
       [console_scripts]
       genbankqc=genbankqc.__main__:cli
-      ''',
-      classifiers=[
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-            'Programming Language :: Python :: 3.6',
-            'Operating System :: POSIX :: Linux',
-            'License :: OSI Approved :: MIT License',
-            'Intended Audience :: Science/Research',
-            'Environment :: Console',
-            'Development Status :: 3 - Alpha'])
+      """,
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+    ],
+)
