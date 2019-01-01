4ef1065f0b68a885b08408f292d53edec3df6596
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-12-31 17:43:38 -0700

    Implement pruning function to remove old assembly versions

--- genbankqc/genbank.py
+++ genbankqc/genbank.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import attr
@@ -37,6 +38,33 @@ class Genbank(object):
     def qc(self):
         for species in self.species():
             species.qc()
+
+    def prune(self):
+        """Prune all files that aren't latest assembly versions."""
+        # patterns for matching accession IDs
+        p_id = re.compile("GCA_[0-9]*.[0-9]")
+        p_glob = "GCA_[0-9]*.[0-9]_*[fasta|msh|csv]"
+
+        # IDs and associated files
+        d_local = {}
+
+        def glob_local():
+            """Yield all files that match `p_glob`"""
+            for path in self.root.rglob(p_glob):
+                id_ = p_id.match(path.name).group()
+                d_local[id_] = []
+                yield path
+
+        # Update `d_local` with a list containing paths for all matches
+        for path in glob_local():
+            id_ = p_id.match(path.name).group()
+            d_local[id_].append(path)
+
+        # Remove local files that aren't latest assembly versions
+        assumbly_summary = metadata.AssemblySummary(self.paths.metadata)
+        for i in set(d_local.keys()) - set(assumbly_summary.ids):
+            for f in d_local[i]:
+                f.unlink()
 
     def biosample_metadata(self, email):
         biosample = metadata.BioSample(self.paths.metadata, email=email)
