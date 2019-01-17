c7763396c03f3e884ce037796470812536472122
Andrew Sanchez <inbox.asanchez@gmail.com>
2019-01-16 17:25:16 -0700

    Test for species metadata

--- test/genbank_test.py
+++ test/genbank_test.py
@@ -1,7 +1,10 @@
+import os
 import shutil
 import pytest
 import tempfile
 from pathlib import Path
+
+import pandas as pd
 
 from genbankqc import Genbank
 from genbankqc import Species
@@ -18,3 +21,12 @@ def test_genbank_init(genbank):
     assert isinstance(genbank, Genbank)
     for i in genbank.species():
         assert isinstance(i, Species)
+
+
+@pytest.mark.skip(reason="Fails because our test genomes aren't in first 1000k samples")
+def test_species_metadata(genbank):
+    metadata = genbank.metadata(email="inbox.asanchez@gmail.com", sample=1000)
+    genbank.species_metadata(metadata)
+    for species in genbank.species():
+        assert isinstance(species.metadata, pd.DataFrame)
+        assert os.path.isfile(species.metadata_path)
