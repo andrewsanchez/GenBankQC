62a8516eac8750a352b32da01b03ae6ac9b85bb9
Andrew Sanchez <inbox.asanchez@gmail.com>
2019-01-17 16:54:07 -0700

    Add some echo statements

--- scripts/efetch_sra_runs.sh
+++ scripts/efetch_sra_runs.sh
@@ -9,6 +9,7 @@ find $metadata_dir -name "*sra_ids*" -or -name "sra_runs.tsv" -delete
 
 # Get SRA runs for each ID
 split -l 5000 ${sra_ids} ${metadata_dir}_sra_ids_
+echo "Downloading runs for each SRA ID..."
 for f in $sra_ids_split;
 do epost -db sra -input $f -format acc | \
         efetch -format docsum | \
@@ -17,3 +18,4 @@ do epost -db sra -input $f -format acc | \
                -group Runs -sep ',' -element Runs/Run@acc \
                >> $sra_runs
 done
+echo "Finished downloading runs"
