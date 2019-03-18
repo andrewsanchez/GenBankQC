01e59d3a2641a4cb1637360d8e1ecfc375d6bf60
Andrew Sanchez <inbox.asanchez@gmail.com>
2019-03-18 15:23:58 -0700

    Clean up and allow for named sub directories

--- genbankqc/config.py
+++ genbankqc/config.py
@@ -8,20 +8,14 @@ class Paths(object):
     subdirs = attr.ib(default=[], validator=attr.validators.instance_of(list))
 
     def __attrs_post_init__(self):
-        self.root.mkdir(exist_ok=True)
-        for subdir in self.subdirs:
-            name = self.clean_path_name(subdir)
-            path = self.root / subdir
-            object.__setattr__(self, name, path)
-        self.mkdirs()
 
-    def mkdirs(self):
-        """Create `root` and `subdirs` if they don't already exist."""
         for subdir in self.subdirs:
-            name = self.clean_path_name(subdir)
-            path = self.__getattribute__(name)
-            path.mkdir(exist_ok=True)
-
-    @staticmethod
-    def clean_path_name(path):
-        return path.strip(".")
+            if isinstance(subdir, tuple):
+                name, path = subdir
+                path = self.root / path
+                path.mkdir(exist_ok=True)
+                object.__setattr__(self, name, path)
+            else:
+                path = self.root / subdir
+                path.mkdir(exist_ok=True)
+                object.__setattr__(self, subdir, path)
